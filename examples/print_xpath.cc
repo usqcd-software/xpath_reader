7b52536b021612f1570d7ad29e65a93e7327f7d9
edwards <edwards>
2005-05-15 21:43:50 +0000

    Changed to fall back to Basic xpath reader.

--- examples/print_xpath.cc
+++ examples/print_xpath.cc
@@ -9,10 +9,16 @@
 using namespace std;
 using namespace XMLXPathReader;
 
+#define USE_BASIC
+
 int main(int argc, char *argv[])
 {
   // Instantiate a reader
+#if defined(USE_BASIC)
+  BasicXPathReader reader;
+#else
   XPathReader reader;
+#endif
  
   if (argc != 3)
   {
@@ -32,12 +38,23 @@ int main(int argc, char *argv[])
   // Try and get a string 
   string sresult ="";  
   try {
+#if defined(USE_BASIC)
+    reader.evaluateXPath(string(argv[2]));
+#else
     reader.getXPath<string>(argv[2], sresult);
     cout << sresult << endl;
+#endif
   }
   catch (const string& e) {
     cout << e << endl;
+    exit(1);
   }
+    
+#if defined(USE_BASIC)
+  // If no error occurred, and also we didn't quit then print the query
+  // results.
+  reader.printQueryResult(cout);
+#endif
 
   /* This bit kills the last refcount */
   reader.close();
